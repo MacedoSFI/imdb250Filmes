80e138a7b4b65872fed187c7acc4ca2562acdaba
Felipe <49687766+MacedoSFI@users.noreply.github.com>
2022-10-12 07:07:15 -0300

    Adicionado o @Autowired
    Foram criados os pacotes modelo e repository. As classes, o banco de dados, o application.properties e a configuração @Autowired.

--- imdb/src/main/java/com/sevenDaysOfCode/imdb/controller/ImdbController.java
+++ imdb/src/main/java/com/sevenDaysOfCode/imdb/controller/ImdbController.java
@@ -3,6 +3,7 @@ package com.sevenDaysOfCode.imdb.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ImdbController {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@GetMapping(value = "/250Filmes")
 	public List<Object> get250Filmes() {
-		String url = "https://imdb-api.com/en/API/Top250Movies/<apiKey>";
-		
-		RestTemplate restTemplate = new RestTemplate();
+		String url = "https://imdb-api.com/en/API/Top250Movies/k_4qe4eo9s";
 		ResponseEntity<String> filmes = restTemplate.getForEntity(url, String.class);
 		System.out.println(Arrays.asList(filmes));
 		return Arrays.asList(filmes);
