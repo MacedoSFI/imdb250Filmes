8f0d14fdd434c65f3ecccf09fbaf6b8ddcaec553
Felipe <49687766+MacedoSFI@users.noreply.github.com>
2022-10-14 20:34:33 -0300

    Desafio do dia 4.
    Não consegui fazer. Muito diferente do que foi ensinado na formação Spring.

--- imdb/src/main/java/com/sevenDaysOfCode/imdb/controller/ImdbController.java
+++ imdb/src/main/java/com/sevenDaysOfCode/imdb/controller/ImdbController.java
@@ -1,6 +1,6 @@
 package com.sevenDaysOfCode.imdb.controller;
 
-import java.util.Arrays;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +9,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.sevenDaysOfCode.imdb.dto.FilmeDto;
-import com.sevenDaysOfCode.imdb.modelo.Filme;
-
 @RestController
 public class ImdbController {
 	
-	//@Autowired
-	//private RestTemplate restTemplate;
-	
-	@GetMapping(value = "/250Filmes")
-	public List<FilmeDto> get250Filmes() {
-		//String url = "https://imdb-api.com/en/API/Top250Movies/k_4qe4eo9s";
-		//ResponseEntity<Filme> filmes = restTemplate.getForObject(url, Filme.class);
-		Filme filme = new Filme(246, "Dersu Uzala", "Dersu Uzala (1975)", "1975", 
-				"https://m.media-amazon.com/images/M/MV5BYWY0OWJlZTgtMWUzNy00MGJhLTk5YzQtNmY5MDEwOTIxNjMyXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX128_CR0,12,128,176_AL_.jpg", 
-				"Akira Kurosawa (dir.), Maksim Munzuk, Yuriy Solomin", 8.0,	29732.0);
-		
-		//Object[] filmes = (Object[]) restTemplate.getForObject(url, Object[].class);
-		//
-		List<Filme> filmes = Arrays.asList(filme, filme, filme);
-		System.out.println(FilmeDto.converter(filmes));
-		return FilmeDto.converter(filmes);
-	}
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @GetMapping("/250Filmes")
+    public ListOfMovies getTop250Filmes() {
+
+        ResponseEntity<ListOfMovies> response =
+                this.restTemplate.getForEntity("https://imdb-api.com/en/API/Top250Movies/k_4qe4eo9s", ListOfMovies.class);
+        return response.getBody();
+    }
+    
+    @GetMapping("/250/Filmes")
+    public void getFilmes() {
+
+        ResponseEntity<ListOfMovies> response =
+                this.restTemplate.getForEntity("https://imdb-api.com/en/API/Top250Movies/k_4qe4eo9s", ListOfMovies.class);
+        PrintWriter writer = new PrintWriter("content.html");
+        new HTMLGenerator(writer).generate(response);
+        
+        writer.close();
+    }
+
+    record Movie(String title, String image, String year, String imDbRating){}
+    record ListOfMovies(List<Movie> items){}
+    
+   
 }
+
