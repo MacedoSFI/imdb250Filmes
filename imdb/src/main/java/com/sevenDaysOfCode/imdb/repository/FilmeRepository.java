2081e6526aa8bcfaf1bf4de9f400c6df3e6dd38b
Felipe <49687766+MacedoSFI@users.noreply.github.com>
2022-10-15 22:55:20 -0300

    Atualizando projeto
    deletando arquivo que já não estão mais no projeto.

--- imdb/src/main/java/com/sevenDaysOfCode/imdb/repository/FilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sevenDaysOfCode.imdb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sevenDaysOfCode.imdb.modelo.Filme;
-
-public interface FilmeRepository extends JpaRepository<Filme, Long>{
-
-}
